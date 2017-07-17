7a688892a9f59f3733ea70e6b87bd116c1350a92
zbsedefian <zbsedefian@gmail.com>
2017-07-16 23:46:36 -0400

    trying to draw two unique decks -- now getting two indentical decks for both users

--- Poker/src/Game.java
+++ Poker/src/Game.java
@@ -1,11 +1,38 @@
+import javax.swing.JOptionPane;
 
 public class Game {
 
 	public static void main(String[] args) {
+		
 		Player user = new Player();
 		Player computer = new Player();
 		
-		Player.createDeck();
+		// create an array (deck) out of the above cards and return
+		
+		Card[] theDeck = Deck.createCards();
+
+		user.drawHand( theDeck );
+		computer.drawHand( theDeck );
+		
+//		//make sure none of the cards in the computer's hand matches the user's hand
+//		for( int i = 0; i < 5; i++){		
+//			for( int j = 0; j < 5; j++){
+//				if(computer.hand[i] == user.hand[j]){
+//					computer.drawHand(theDeck);
+//				}
+//			}
+//		}
+		
+		
+		
+		JOptionPane.showMessageDialog(null, "Your hand: \n" + user.hand[0].cardName + '\n' + user.hand[1].cardName + '\n' +
+											user.hand[2].cardName + '\n' + user.hand[3].cardName + '\n' + 
+											user.hand[4].cardName + '\n');
+		
+		JOptionPane.showMessageDialog(null, "Computer's hand: \n" + computer.hand[0].cardName + '\n' + computer.hand[1].cardName + '\n' +
+											computer.hand[2].cardName + '\n' + computer.hand[3].cardName + '\n' + 
+											computer.hand[4].cardName + '\n');
+				
 	}
 
 }
--- Poker/src/Player.java
+++ Poker/src/Player.java
@@ -1,53 +1,50 @@
+
+
 import java.util.Random;
 
 public class Player {
 
 	private int balance = 0;
-	private static Card[] hand = new Card[5];
+	public static Card[] hand = new Card[5];
 	
-	public static void createDeck() {
+	public static Card[] drawHand( Card[] theDeck) {
 		
-		// create a deck using the above card objects in random order
-	
-		Card[] theDeck = Deck.createCards();
+		// create a deck
 		
 		
-		
-		// make selection a random number from 0 to 51
+		// make 'selection' variable a random number from 0 to 51
 		int selection;
 		final int upperBound = 51;
 		final int lowerBound = 0;
-		
 		Random generator = new Random();
 		
-		selection = generator.nextInt();
-		selection = Math.abs(selection);
-		selection %= ( upperBound - lowerBound );
-		selection += lowerBound;
-		
-		
-		
 		for( int i = 0; i < 5; i++ ){
-			
-			do{
-				hand[i] = theDeck[selection];
-			} while (hand[0] != theDeck[selection] && hand[1] != theDeck[selection] && 
-				 hand[2] != theDeck[selection] &&  hand[3] != theDeck[selection] && 
-				 hand[4] != theDeck[selection] );
 		
+			selection = generator.nextInt();
+			selection = Math.abs(selection);
+			selection %= ( upperBound - lowerBound );
+			selection += lowerBound;
+				
+			// make sure the card hasn't already been selected
 			
+			while ( hand[0] == theDeck[selection] || hand[1] == theDeck[selection] ||
+	 				hand[2] == theDeck[selection] ||  hand[3] == theDeck[selection] ||
+	 				hand[4] == theDeck[selection] ) {
+				
+//				System.out.println("already selected...");
+				selection = generator.nextInt();
+				selection = Math.abs(selection);
+				selection %= ( upperBound - lowerBound );
+				selection += lowerBound;
+				
+			}
 			
+			hand[i] = theDeck[selection];
+			
+//  			System.out.println( selection );
+  				
 		}
 		
-		System.out.println( hand[0].cardName + "'" );
-		
-		
-		
-		
-		
-		
-	}
-	
-	
-	
+		return hand;
+	}	
 }
